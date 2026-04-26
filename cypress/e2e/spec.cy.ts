4880babbee17871330caa106500a9b0475cb5176
Tatiane Moraes <tatiane.moraes@checkstore.com.br>
2026-04-26 20:52:09 -0300

    test: adicionar teste E2E de botão desabilitado com estoque zerado

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -9,19 +9,27 @@ describe('Vortex Cart', () => {
     })
     cy.visit('/')
   });
+
   it('deve exibir a lista de produtos', () => {
     cy.get('.product-card').should('have.length.at.least', 1)
   });
+
   it('deve adicionar um produto ao carrinho', () => {
     cy.get('.btn-add').first().click();
     cy.get('.cart-item').should('have.length.at.least', 1)
   });
+
   it('deve remover um produto do carrinho', () => {
     cy.get('.btn-add').first().click();
     cy.get('.cart-item').should('have.length.at.least', 1);
     cy.get('.btn-remove').first().click();
     cy.get('.cart-item').should('have.length', 0)
+  });
 
-  })
-
+  it('deve desabilitar o botão quando o produto está esgotado', () => {
+    Cypress._.times(5, () => {
+      cy.get(".btn-add").eq(1).click();
+    });
+    cy.get('.btn-add').eq(1).should('be.disabled');
+  });
 });
