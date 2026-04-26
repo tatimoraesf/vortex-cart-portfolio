51290bba5b51f0d4113fae18e36931b01e96c350
Tatiane Moraes <tatiane.moraes@checkstore.com.br>
2026-04-26 20:31:22 -0300

    test: adicionar teste E2E de adicionar produto ao carrinho

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,12 @@
 describe('Vortex Cart', () => {
   beforeEach(() => {
     cy.visit('/')
-  })
+  });
   it('deve exibir a lista de produtos', () => {
     cy.get('.product-card').should('have.length.at.least', 1)
+  });
+  it('deve adicionar um produto ao carrinho', () => {
+    cy.get('.btn-add').first().click();
+    cy.get('.cart-item').should('have.length.at.least', 1)
   })
-})
+});
