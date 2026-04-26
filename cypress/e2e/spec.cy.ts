87c6d8615dff9d06cdfe7b97235aa5dfcb8e9af1
Tatiane Moraes <tatiane.moraes@checkstore.com.br>
2026-04-26 20:46:02 -0300

    test: adicionar testes E2E de adicionar e remover produto do carrinho

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,12 @@
 describe('Vortex Cart', () => {
   beforeEach(() => {
+    cy.request({
+      method: 'POST',
+      url: '/admin/reset-db',
+      headers: {
+        Authorization: `Bearer ${Cypress.env('ADMIN_API_KEY')}`
+      }
+    })
     cy.visit('/')
   });
   it('deve exibir a lista de produtos', () => {
@@ -8,5 +15,13 @@ describe('Vortex Cart', () => {
   it('deve adicionar um produto ao carrinho', () => {
     cy.get('.btn-add').first().click();
     cy.get('.cart-item').should('have.length.at.least', 1)
+  });
+  it('deve remover um produto do carrinho', () => {
+    cy.get('.btn-add').first().click();
+    cy.get('.cart-item').should('have.length.at.least', 1);
+    cy.get('.btn-remove').first().click();
+    cy.get('.cart-item').should('have.length', 0)
+
   })
+
 });
