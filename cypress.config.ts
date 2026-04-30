062412c0fb9dc71e4841ae847c0714fe2824836a
Tatiane Moraes <tatiane.moraes@checkstore.com.br>
2026-04-30 20:34:38 -0300

    feat: adicionar cypress-mochawesome-reporter para relatório de testes E2E

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: true,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
 
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
